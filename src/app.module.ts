d5638fad87a01ec24dc9d62e1dbb1524ea9e8dba
Osouzaa <gabrielsouzaalves750@gmail.com>
2025-05-26 11:21:45 -0300

    feat: config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { DataBaseModule } from './database/database.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    DataBaseModule],
   controllers: [],
   providers: [],
 })
---
+++ src/database/database.config.ts
@@ -0,0 +1,23 @@
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from "@nestjs/common";
+import { env } from "src/env";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: env.DB_HOST,
+      port: env.DB_PORT,
+      username: env.DB_USERNAME,
+      password: env.DB_PASSWORD,
+      database: env.DB_NAME,
+      entities: [],
+      options: {
+        encrypt: false, // importante se estiver rodando localmente
+        trustServerCertificate: true,
+      },
+      synchronize: true
+    }),
+  ],
+})
+export class DataBaseModule { }

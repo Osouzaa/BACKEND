b23f2e90c605508e6a689cc8dd42d8ed9b04b8bd
neto <picilianovasconcelos@gmail.com>
2025-05-27 20:41:39 -0300

    refactor: remove registration method

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-auth.dto';
 
 @Controller('auth')
@@ -10,10 +9,5 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: LoginUserDto) {
     return this.authService.login(loginDto);
-  }
-
-  @Post('register')
-  async register(@Body() registerUserDto: RegisterUserDto) {
-    return this.authService.register(registerUserDto);
   }
 }

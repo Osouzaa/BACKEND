a77cda44e5042b160efc1678449117876471748c
neto <picilianovasconcelos@gmail.com>
2025-05-27 14:00:42 -0300

    feat: include AuthModule in AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DataBaseModule } from './database/database.config';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { UsersModule } from './modules/users/users.module';
       isGlobal: true
     }),
     DataBaseModule,
-    UsersModule],
+    UsersModule,
+    AuthModule],
   controllers: [],
   providers: [],
 })

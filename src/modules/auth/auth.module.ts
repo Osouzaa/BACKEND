ec2d5656caae7af367eda928c83585cf5c08ed5c
neto <picilianovasconcelos@gmail.com>
2025-05-28 11:20:28 -0300

    refactor: replace ConfigService with static env config for JWT setup

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -7,23 +7,18 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { JwtModule } from '@nestjs/jwt';
+import { Candidate } from '../candidates/entities/candidate.entity';
+import { env } from 'src/config/env';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
-    ConfigModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('SECRET_KEY'),
-        signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
-        },
-      }),
+    TypeOrmModule.forFeature([User, Candidate]),
+    JwtModule.register({
+      secret: env.SECRET_KEY,
+      signOptions: {
+        expiresIn: env.JWT_EXPIRES_IN,
+      },
     }),
   ],
   controllers: [AuthController],

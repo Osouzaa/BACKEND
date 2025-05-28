ee63300f499f7badf369f7397429eab446ac733a
neto <picilianovasconcelos@gmail.com>
2025-05-28 11:24:07 -0300

    feat: secure findAll endpoint for roles 'candidate' and 'admin'

--- src/modules/candidates/candidates.controller.ts
+++ src/modules/candidates/candidates.controller.ts
@@ -1,17 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { CandidatesService } from './candidates.service';
 import { CreateCandidateDto } from './dto/create-candidate.dto';
 import { UpdateCandidateDto } from './dto/update-candidate.dto';
+import { RolesGuard } from '../auth/guards/jwt-guard.role';
+import { Roles } from 'src/decorators/roles.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('candidates')
 export class CandidatesController {
-  constructor(private readonly candidatesService: CandidatesService) { }
+  constructor(private readonly candidatesService: CandidatesService) {}
 
   @Post()
   create(@Body() createCandidateDto: CreateCandidateDto) {
     return this.candidatesService.create(createCandidateDto);
   }
 
+  //Exemplo de uso (guards e roles):
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('candidate')
   @Get()
   findAll() {
     return this.candidatesService.findAll();
@@ -23,7 +38,10 @@ export class CandidatesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCandidateDto: UpdateCandidateDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCandidateDto: UpdateCandidateDto,
+  ) {
     return this.candidatesService.update(id, updateCandidateDto);
   }
 

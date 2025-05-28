8811e93f085b08a2a5b5029755d8cf4072f5b153
Osouzaa <gabrielsouzaalves750@gmail.com>
2025-05-28 19:48:01 -0300

    feat(experience): create Experience entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from './modules/users/users.module';
 import { CandidatesModule } from './modules/candidates/candidates.module';
 
 import { AuthModule } from './modules/auth/auth.module';
-import { ExperienceModule } from './experience/experience.module';
+import { ExperienceModule } from './modules/experience/experience.module';
 
 @Module({
   imports: [
